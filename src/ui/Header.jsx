a9e54ef9ebab708b3b921dafd2ed0eda7e32f00a
mahdiShojaei <s.mhd.shojaei@gmail.com>
2024-02-13 15:11:02 +0330

    1

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,6 @@ import useUser from '../pages/useUser';
 
 function Header() {
 	const { data } = useUser();
-	console.log(data);
 	return <div className='bg-secondary-0 py-4 px-8'>app header</div>;
 }
 
